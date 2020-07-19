d7761bd680aa4f5873690f2b6ed23111dbd205d9
ammanvedi <amman.vedi@gmail.com>
2020-07-19 21:54:17 +0100

    add more tests

--- spago.dhall
+++ spago.dhall
@@ -4,7 +4,8 @@ You can edit this file as you like.
 -}
 { name = "my-project"
 , dependencies =
-  [ "assert"
+  [ "arrays"
+  , "assert"
   , "console"
   , "datetime"
   , "effect"
--- test/DateHelpers.purs
+++ test/DateHelpers.purs
@@ -1,8 +1,12 @@
 module Test.Main where
 
+import Data.Date
+import Data.Enum
+import Data.Maybe
+import Effect.Now
 import Prelude
 
-import DateHelpers (getStartOfMonth)
+import DateHelpers (getStartOfMonth, getXDaysPrior, maybeDateToString)
 import Effect (Effect)
 import Effect.Aff (launchAff_, delay)
 import Effect.Class.Console (log)
@@ -10,15 +14,11 @@ import Test.Spec (pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
-import Effect.Now
-import Data.Enum
-import Data.Date
-import Data.Maybe
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
   describe "Date Helpers" do
-    describe "Get Start Of Month" do
+    describe "getStartOfMonth" do
       it "Returns the first of the month" do
         testYear <- pure $ (fromMaybe bottom $ toEnum 1994)
         testDay <- pure $ (fromMaybe bottom $ toEnum 4)
@@ -29,4 +29,18 @@ main = launchAff_ $ runSpec [consoleReporter] do
         case result of
           Nothing -> false `shouldEqual` true
           (Just d) -> (day d) `shouldEqual` firstDay
-        
+    describe "getXDaysPrior" do
+      it "returns x days prior" do
+        testYear <- pure $ (fromMaybe bottom $ toEnum 1994)
+        testDay <- pure $ (fromMaybe bottom $ toEnum 4)
+        testMonth <- pure July
+        date <- pure $ canonicalDate testYear testMonth testDay
+        targetDay <- pure $ (fromMaybe bottom $ toEnum 14)
+        targetYear :: Year <- pure $ (fromMaybe bottom $ toEnum 1994)
+        result <- pure $ getXDaysPrior (Just date) (-20)
+        case result of
+          Nothing -> false `shouldEqual` true
+          (Just d) -> do
+              (day d) `shouldEqual` targetDay
+              (month d) `shouldEqual` June
+              (year d) `shouldEqual` targetYear

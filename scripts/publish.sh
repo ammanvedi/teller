f97683cd5e117f3213096f7fd4f7f07120b8a464
ammanvedi <amman.vedi@gmail.com>
2020-08-31 21:28:37 +0100

    update publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,8 @@ sh ./scripts/build.sh > /dev/null
 echo "JS built!"
 echo "Bumping $bumpVersion version"
 npm --no-git-tag-version version $bumpVersion
-git add .
-git commit -m "update package json version"
+git add . --quiet
+git commit -m "update package json version" --quiet
 spago bump-version $bumpVersion --no-dry-run
+git push --follow-tags --quiet
 pulp publish

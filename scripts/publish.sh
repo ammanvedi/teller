4dd73dcdccd112747bc5f38a6561a558375058fb
ammanvedi <amman.vedi@gmail.com>
2020-08-31 21:20:54 +0100

    updates to build script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,6 +4,6 @@ echo "Building Javascript..."
 sh ./scripts/build.sh > /dev/null
 echo "JS built!"
 echo "Bumping $bumpVersion version"
-spago bump-version $bumpVersion
+spago bump-version $bumpVersion --no-dry-run
 npm version from-git
 pulp publish

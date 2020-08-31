c414bd707931425fc31b4cad5088b6dce8cdd8a7
ammanvedi <amman.vedi@gmail.com>
2020-08-31 23:37:05 +0100

    update package json version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -34,6 +34,30 @@ export class EveryWeekdayTrendDescription {}
 
 export class WeekendTrendDescription {}
 
+export function isMonthDayTrendDescription(
+  inst: any
+): inst is MonthDayTrendDescription;
+
+export function isLastWeekdayTrendDescription(
+  inst: any
+): inst is LastWeekdayTrendDescription;
+
+export function isSpecificWeekdayTrendDescription(
+  inst: any
+): inst is SpecificWeekdayTrendDescription;
+
+export function isWeekdayTrendDescription(
+  inst: any
+): inst is WeekdayTrendDescription;
+
+export function isEveryWeekdayTrendDescription(
+  inst: any
+): inst is EveryWeekdayTrendDescription;
+
+export function isWeekendTrendDescription(
+  inst: any
+): inst is WeekendTrendDescription;
+
 type TrendDescription =
   | MonthDayTrendDescription
   | LastWeekdayTrendDescription

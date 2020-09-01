b0673152f17b2e74b1a63f21ebf03449fd1f17f8
ammanvedi <amman.vedi@gmail.com>
2020-09-01 06:59:40 +0100

    add message to deploy to pursuit instead of doing it from bash

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,5 +10,4 @@ git commit -m "update package json version"
 spago bump-version $bumpVersion --no-dry-run
 git push --follow-tags
 npm publish
-pwd
-pulp publish --no-push
+echo "PLEASE RUN \"pulp publish --no-push\" to publish to pursuit"

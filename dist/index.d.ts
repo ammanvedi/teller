b5f3440f9b506b16609c0e5001ae9d81ef51a30b
ammanvedi <amman.vedi@gmail.com>
2020-09-01 21:01:34 +0100

    update package json version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -11,30 +11,45 @@ export class TrendDescription {}
 export class MonthDayTrendDescription extends TrendDescription {
   public value0: {
     dayOfMonth: number;
+    pricing: number;
   };
 }
 
 export class LastWeekdayTrendDescription extends TrendDescription {
   public value0: {
     weekday: number;
+    pricing: number;
   };
 }
 
 export class SpecificWeekdayTrendDescription extends TrendDescription {
   public value0: {
     weekday: number;
+    pricing: number;
   };
 }
 
 export class WeekdayTrendDescription extends TrendDescription {
   public value0: {
+    // Monday = 1, Sunday = 7
     weekdays: Array<number>;
+    pricing: Array<number>;
   };
 }
 
-export class EveryWeekdayTrendDescription extends TrendDescription {}
+export class EveryWeekdayTrendDescription extends TrendDescription {
+  public value0: {
+    // mon, tue, wed, thu, fri
+    pricing: [number, number, number, number, number];
+  };
+}
 
-export class WeekendTrendDescription extends TrendDescription {}
+export class WeekendTrendDescription extends TrendDescription {
+  public value0: {
+    // sat, sun
+    pricing: [number, number];
+  };
+}
 
 export function isMonthDayTrendDescription(
   inst: TrendDescription

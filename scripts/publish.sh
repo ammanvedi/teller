11c23fea91061db9f2bcb9a977caccf6b4c3cad2
ammanvedi <amman.vedi@gmail.com>
2020-09-01 06:56:41 +0100

    debug publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# bumpVersion=$1
-# echo "Building Javascript..."
-# sh ./scripts/build.sh > /dev/null
-# echo "JS built!"
-# echo "Bumping $bumpVersion version"
-# npm --no-git-tag-version version $bumpVersion
-# git add .
-# git commit -m "update package json version"
-# spago bump-version $bumpVersion --no-dry-run
-# git push --follow-tags
-# npm publish
-# pwd
+bumpVersion=$1
+echo "Building Javascript..."
+sh ./scripts/build.sh > /dev/null
+echo "JS built!"
+echo "Bumping $bumpVersion version"
+npm --no-git-tag-version version $bumpVersion
+git add .
+git commit -m "update package json version"
+spago bump-version $bumpVersion --no-dry-run
+git push --follow-tags
+npm publish
 pwd
 pulp publish --no-push
